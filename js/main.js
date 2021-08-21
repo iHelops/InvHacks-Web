22d8e3010990a2a8e9193e29ec656f463bc35a5b
Helops <iHelops@yandex.ru>
2021-08-21 19:29:00 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('a[href^="#"]').on('click', function(event) {
+      event.preventDefault();
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $($(this).attr("href")).offset().top
+    }, 800);
+    });
+  });
